236d9acec038ee15376f61d9acb8564a8f76028a
MrFalso <salillari.tedi@gmail.com>
2024-05-27 15:48:10 +0200

    new

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,34 @@
 const app = require('express')();
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://dottorciccio03:DetectiveCiccio03@esame-pwm.usssncc.mongodb.net/";
+
+const client = new MongoClient(uri); 
 
 let port = 3000;
 
+async function loginUser(res, body) {
+    //let {email, password } = body; 
+
+    //console.log((body.email === undefined ? 'Email mancante' : body.email));
+
+    try {
+        await client.connect();
+        //let user = await client.db('Users').collection('user').findOne({email: body.email, password: body.password});
+        //console.log(user);
+    } finally {
+        await client.close();
+    }
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.params.name);
+    loginUser(res, req.body)
+        .catch((err) => console.log(err));
 })
 
 app.listen(port, () => {
